abb858d9cf65d1631cbab5690fcee4002c657b45
Liam Pulles <me@liampulles.com>
2020-09-17 23:57:48 +0200

    Include args

--- pkg/app/run.go
+++ pkg/app/run.go
@@ -1,10 +1,43 @@
 package app
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+type programArgs struct {
+	Sleep int
+	URL   string
+}
 
 // Run is the entrypoint for hoke. We delegate logic here
 // so that it can be tested.
 func Run(args []string) int {
-	fmt.Println("I got my teeth back.")
+	progArgs, res := extractArgs(args)
+	if res != 0 {
+		return res
+	}
+
+	fmt.Printf("Checking %s every %d seconds\n", progArgs.URL, progArgs.Sleep)
 	return 0
+}
+
+func extractArgs(args []string) (*programArgs, int) {
+	flagSet := flag.NewFlagSet("hoke", flag.ContinueOnError)
+	sleepPtr := flagSet.Int("sleep", 5, "Sleep duration")
+	if err := flagSet.Parse(args[1:]); err != nil {
+		fmt.Fprintf(os.Stderr, "flag parse error: %v\n", err)
+		return nil, 1
+	}
+	if flagSet.NArg() != 1 {
+		fmt.Fprintf(os.Stderr, "positional arg error: expecting 1 positional argument for the url, but got %d\n", flagSet.NArg())
+		return nil, 2
+	}
+	url := flagSet.Arg(0)
+
+	return &programArgs{
+		Sleep: *sleepPtr,
+		URL:   url,
+	}, 0
 }
--- test/pkg/app/run_test.go
+++ test/pkg/app/run_test.go
@@ -7,10 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRun_SmokeTest(t *testing.T) {
+func TestRun_WhenGivenValidArgs_ShouldPass(t *testing.T) {
+	// Exercise SUT
+	actual := app.Run([]string{"hoke", "https://github.com"})
+
+	// Verify results
+	assert.Equal(t, 0, actual)
+}
+
+func TestRun_WhenGivenInvalidArgs_ShouldFail(t *testing.T) {
+	// Exercise SUT
+	actual := app.Run([]string{"hoke", "-invalid"})
+
+	// Verify results
+	assert.Equal(t, 1, actual)
+}
+
+func TestRun_WhenMissingUrl_ShouldFail(t *testing.T) {
 	// Exercise SUT
 	actual := app.Run([]string{"hoke"})
 
 	// Verify results
-	assert.Equal(t, 0, actual)
+	assert.Equal(t, 2, actual)
 }
